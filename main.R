7bf94ed77e3ba24654037722deabe89e3f9f2051
Jared Zystro <jaredzystro@Jareds-MacBook-Pro.local>
2016-09-21 11:22:39 -0700

    Began creating workflow file

---
+++ main.R
@@ -0,0 +1,29 @@
+
+### R Workflow for data analysis and prediction of 
+### Jared Zystro's se sweet corn breeding project
+
+# PART A - for each trait
+
+# 1. Import data
+#     Load in 2015 and 2016 phenotype data
+#     Load in marker data
+
+# 2. Format data
+#     Set factors
+
+# 3. Quality control
+#     Check for outliers
+#     Test for normality
+#     Test for equal variance
+
+# 4. Test for experient-wide GxE
+#     ANOVA
+#     Spearman rank correlation
+
+# 5. Test to see if means should be adjusted based on checks
+
+# 6. ANOVAs
+
+
+
+
