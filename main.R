80ef3179e1bcd0ae781c0818b4b31bfc6254f905
Jared Zystro <jaredzystro@Jareds-MacBook-Pro.local>
2016-09-21 13:13:54 -0700

    First draft outline

--- main.R
+++ main.R
@@ -1,15 +1,18 @@
 
-### R Workflow for data analysis and prediction of 
+### Workflow for data analysis and prediction of 
 ### Jared Zystro's se sweet corn breeding project
 
 # PART A - for each trait
 
-# 1. Import data
+# 1. Import / create data tables
 #     Load in 2015 and 2016 phenotype data
-#     Load in marker data
+#     Load in inbred marker data
+#     Create pedigree table
+#     Create marker data for hybrids from inbred marker data
 
 # 2. Format data
-#     Set factors
+#     Set factors in phenotype data
+#     Format marker data
 
 # 3. Quality control
 #     Check for outliers
@@ -21,8 +24,48 @@
 #     Spearman rank correlation
 
 # 5. Test to see if means should be adjusted based on checks
+#     ANOVAs of checks
 
 # 6. ANOVAs
+#     Inbred ANOVAs
+#     Hybrid ANOVAs
+ 
+# 7. Collect phenotype values
+#     Inbred means
+#     Tested hybrid means
+#     Inbred GCAs
+#     Tested hybrid SCAs
+
+# 8. Predict untested hybrid means
+#     Classical (u + GCAf + GCAm)
+#     GBLUP
+#     Cross validiate GBLUP
+#     Classical vs. GBLUP comparison
+
+# 9. Predict synthetics
+#     Classical (two step)
+#     GBLUP (two step)
+#     GLBUP (one step)
+
+# PART B - for all traits
+
+# 10. Merge trait data
+
+# 11. Filter and sort hybrids and synthetics based on index
+
+# 12. Create tables and charts
+#      QC info - QQplots, boxplots, etc
+#      ANOVA tables
+#      Spearkman correlations
+#      Inbred means
+#      Tested hybrid means
+#      Inbred GCAs
+#      Tested hybrid SCAs
+#      Predicted hybrid means (Classical and GBLUP)
+#      GBLUP cross-validation 
+#      Predicted synthetic means (filtered)
+
+
 
 
 
